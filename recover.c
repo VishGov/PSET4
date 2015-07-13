04ee4d18b3d6cf961a1cdd62b9fad0e8c4456241
VishGov <vishnugovindan@gmail.com>
2015-07-12 22:27:28 -0700

    Update recover.c

--- recover.c
+++ recover.c
@@ -9,46 +9,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cs50.h>
+#include <stdint.h>
 
 #define CARD "card.raw"
+//typedef uint8_t BYTE;
 
 int main()
 {
-    //	open card.raw.
+    //  open card.raw.
     FILE* card = fopen(CARD,"r");
 
     // initialise index for jpeg file number.
-        int file_num = 0;
+    int file_num = 0;
 
-    //	check if it is empty of if any error occurs.
+    // to check if jpeg is found.
+    int jpeg_found = 0;
+
+    //  check if it is empty of if any error occurs.
     if(card == NULL)
     {
-    	printf("ERROR\n");
-    	return 1;
+        printf("ERROR\n");
+        return 1;
 
     }
-    //	if not, continue.
+    //  if not, continue.
     else
     {
-		//  initialise a file.
+        //  initialise a file.
         FILE* jpeg = fopen("000.jpg","w");
 
+        //  assign an array of 512 bytes to store the read block size.
+        unsigned char buffer[512];
+
+        //  read 512 byte block into buffer.
+        fread(buffer, 512, 1, card);
         
-        while(!feof(card))
-		{
-            //  assign an array of 512 bytes to store the read block size.
-            char* buffer = malloc(512);
-
-            //  read 512 byte block into buffer.
-            fread(buffer, 512, 1, card);
-
+        int start = 0;
+        while(fread(buffer, 512, 1, card) == 1)
+        {
+           
             //  check the starting bytes of block for JPEG starting bytes.
-            if(  buffer[0] == (char)0xff && buffer[1] == (char)0xd8 && buffer[2]  == (char)0xff && ( buffer[3] == (char)0xe0 || buffer[3] == (char)0xe1) )
+            if(  buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2]  == 0xff && ( buffer[3] == 0xe0 || buffer[3] == 0xe1) )
             {
-                    // close previous file.
-                    fclose(jpeg);
-
+                    if(jpeg_found == 1)
+                        // close previous file.
+                        fclose(jpeg);
+                    
                     // open new jpeg file to write into. 
                     char filename[sizeof ("000.jpg")];
                     if(file_num < 10)
@@ -57,22 +63,28 @@ int main()
                         sprintf(filename,"0%d.jpg",file_num);
 
                     jpeg = fopen(filename,"w");
+
+                    // indicate jpeg has been found.
+                    jpeg_found = 1;
             
                     //  increase file number for the next file.
                     file_num++; 
 
             }
             
-            //  write to the opened jpeg.
-            fwrite(buffer, 512, 1, jpeg);
+            //  write to the opened jpeg if one is found.
+            if(jpeg_found == 1)
+               fwrite(buffer, 512, 1, jpeg);
 
-            //  free current buffer to take in next block.
-  	    	free(buffer);
-	  	
-		}
+            if(start == 0)
+                {
+                    fclose(jpeg);
+                    start = 1;
+                }
+        }
         //  close the last jpeg when end of file reached.
-        fclose(jpeg);		
-    	
+        fclose(jpeg);       
+        
     }
     //  close card.raw.
     fclose(card);
